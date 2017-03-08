888bac8ee2eebc531de0f90c3a3eb213d2e7c378
Félix Saparelli <felix@passcod.name>
2017-03-08 14:20:27 +1300

    Better logger, maintenance mode message

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,22 @@
-FROM node:6
+FROM node:6-alpine
 EXPOSE 5000
 
-RUN apt update && apt install -y build-essential curl git sudo
+#RUN apt update && apt install -y build-essential curl git sudo
+RUN apk add --update --no-cache build-base curl file git sudo
 RUN curl -sL https://static.rust-lang.org/rustup.sh -o /rustup.sh \
-    && sh /rustup.sh --spec=stable -y \
+    && sh /rustup.sh --prefix=/usr/local --spec=stable -y \
     && rustc --version \
     && cargo --version \
     && mkdir /app
 
 ENV NODE_ENV production
-
-COPY . /app/
 WORKDIR /app
-RUN npm install && npm run install
 
+COPY package.json .
+RUN npm install --no-scripts
+
+COPY native ./native
+RUN npm run install
+
+COPY app index.js lib public views ./
 ENV RUST_LOG info
--- app/logger.js
+++ app/logger.js
@@ -1,18 +1,42 @@
-const bytes = require('bytes')
 const chalk = require('chalk')
+const formatter = require('format-number')
 const morgan = require('morgan')
 const ms = require('ms')
 
-module.exports = morgan((tokens, req, res) => [
-  [
-    chalk.bold('←'),
+module.exports = morgan((tokens, req, res) => {
+  const time = ms(+tokens['response-time'](req, res))
+  const amount = parseFloat(time)
+  const unit = /[\d.]+(.+)$/.exec(time)[1]
+
+  const format = formatter({
+    suffix: unit.length < 2 ? ` ${unit}` : unit,
+    noSeparator: true,
+    padLeft: 3,
+    padRight: 3,
+    round: 3
+  })
+
+  let i = 0
+  let elapsed = format(amount).split('')
+  while (elapsed[i] === '0' && elapsed[i + 1] !== '.') {
+    elapsed[i] = ' '
+    i += 1
+  }
+
+  elapsed = elapsed.join('')
+
+  const status = +tokens.status(req, res)
+  const colour = status > 400
+    ? 'red'
+    : (status >= 200 && status < 300)
+      ? 'green'
+      : 'blue'
+
+  return [
+    chalk.cyan(elapsed),
     chalk.magenta(tokens.method(req, res)),
-    tokens.url(req, res)
-  ].join(' '),
-  [
     chalk.bold('→'),
-    chalk.magenta(tokens.status(req, res)),
-    '(' + chalk.cyan(bytes.format(+tokens.res(req, res, 'content-length'))) + ')',
-    '(' + chalk.blue(ms(+tokens['response-time'](req, res))) + ')'
+    chalk[colour](status),
+    tokens.url(req, res)
   ].join(' ')
-].join('\n'))
+})
