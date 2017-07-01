e1c70f7230a96879111fb003c1f832252ee5aed2
Félix Saparelli <felix@passcod.name>
2017-07-02 01:33:48 +1200

    Move test posts around

--- test/binding/load.test.js
+++ test/binding/load.test.js
@@ -1,11 +1,10 @@
 const { load } = require('../../native')
-const path = require('path')
 const t = require('tap')
 
 t.test((t) => {
   t.plan(2)
 
-  const all = load(path.resolve('./test/posts/'))
+  const all = load(process.env.BLOGRAPH_POSTS)
   t.equal(all.toArray().length, 5, 'there are some files here')
   t.equal(all.toArray()[0].slug(), '2013/aug/10/grow', 'that happen to have a name')
 })
