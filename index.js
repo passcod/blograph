a2310c7341ef2e4f578f97b088648069a4fb9d25
Félix Saparelli <felix@passcod.name>
2017-07-02 01:55:53 +1200

    Enable listening on socket

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const app = require('./app')
+const { LISTEN_FD, PORT } = process.env
 
-const server = app.listen(process.env.PORT || 5000, () =>
-  console.log(`Listening on port ${server.address().port}`)
-)
+if (LISTEN_FD) {
+  app.listen({ fd: +LISTEN_FD }, () =>
+    console.log('Listening on socket')
+  )
+} else {
+  const server = app.listen(PORT || 5000, () =>
+    console.log(`Listening on port ${server.address().port}`)
+  )
+}
