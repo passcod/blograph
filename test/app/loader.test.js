02e43ef639d9e6b4f46e2fd096c54a8961399b71
Félix Saparelli <felix@passcod.name>
2017-07-01 16:43:18 +1200

    Test app/loader#reloadPosts

--- test/app/loader.test.js
+++ test/app/loader.test.js
@@ -1,14 +1,45 @@
 const chalk = require('chalk')
-const { initialLoadError } = require('../../app/loader')
+const { initialLoadError, reloadPosts } = require('../../app/loader')
 const sinon = require('sinon')
 const t = require('tap')
 
 t.test('reclone')
-t.test('reloadPosts')
+
+t.test('reloadPosts', (t) => {
+  t.plan(6)
+
+  const posts = process.env.BLOGRAPH_POSTS
+  const chalked = chalk.enabled
+  chalk.enabled = false
+  sinon.stub(console, 'log')
+
+  const app = { set: sinon.spy() }
+
+  reloadPosts(app)
+
+  t.ok(app.set.called)
+  t.ok(console.log.calledWithMatch(/Loading posts/))
+  t.ok(console.log.calledWithMatch(/Done loading posts/))
+
+  app.set.reset()
+  console.log.reset()
+
+  delete process.env.BLOGRAPH_POSTS
+  reloadPosts(app)
+
+  t.ok(app.set.called)
+  t.ok(console.log.calledWithMatch(/Loading posts/))
+  t.ok(console.log.calledWithMatch(/Done loading posts/))
+
+  console.log.restore()
+  chalk.enabled = chalked
+  process.env.BLOGRAPH_POSTS = posts
+})
 
 t.test('initialLoadError', (t) => {
   t.plan(4)
-  const chalked = !!chalk.enabled
+
+  const chalked = chalk.enabled
   chalk.enabled = false
   sinon.stub(console, 'error')
   sinon.stub(process, 'exit')
