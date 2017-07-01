584f794b6c42eb801b95b6c64d414b675e8c2d2f
Félix Saparelli <felix@passcod.name>
2017-07-01 13:45:11 +1200

    Test app/view

--- test/app/view.test.js
+++ test/app/view.test.js
@@ -1,7 +1,20 @@
 const view = require('../../app/view')
+const sinon = require('sinon')
 const t = require('tap')
 
-t.test('view')
+t.test('view', (t) => {
+  t.plan(4)
+
+  const res = { render: sinon.spy() }
+  const next = sinon.spy()
+  view({}, res, next)
+  res.view('foobar')
+
+  t.type(res.view, Function)
+  t.equal(res.view.length, 1, 'only one required argument')
+  t.ok(res.render.called)
+  t.ok(next.called)
+})
 
 t.test('htmlstrip', (t) => {
   t.plan(1)
