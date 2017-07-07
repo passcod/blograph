814bc03c6a3caf822cdbcf75c6a0ae842b7b4e6a
Félix Saparelli <felix@passcod.name>
2017-07-07 21:29:06 +1200

    Inline first font ref

--- app/view.js
+++ app/view.js
@@ -1,8 +1,9 @@
+const fs = require('fs')
 const moment = require('moment')
 const plur = require('plur')
 const sanitize = require('sanitize-html')
-const words = require('number-to-words').toWords
 const { version } = require('../package.json')
+const words = require('number-to-words').toWords
 
 function htmlstrip (html) {
   return sanitize(html, {
@@ -15,10 +16,20 @@ function asset (file) {
   return `/assets/${version}/${file}`
 }
 
+const inlines = {}
+function inline (file) {
+  if (!inlines[file]) {
+    inlines[file] = fs.readFileSync(`./assets/${file}`)
+  }
+
+  return inlines[file]
+}
+
 function view (req, res, next) {
   res.view = (partial, locals = {}) => res.render('layout', {
     asset,
     htmlstrip,
+    inline,
     locals,
     moment,
     partial,
