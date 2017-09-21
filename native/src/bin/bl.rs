7f60598552cc8aa29264b94a5b245ed7483b301d
Félix Saparelli <felix@passcod.name>
2017-09-21 16:47:54 +1200

    Add bl utility

---
+++ native/src/bin/bl.rs
@@ -0,0 +1,18 @@
+extern crate list;
+#[macro_use] extern crate log;
+extern crate post;
+extern crate walkdir;
+extern crate yaml_rust;
+
+use std::env;
+
+#[path = "../all.rs"]
+mod all;
+
+fn main() {
+    let posts = all::load(env::current_dir().unwrap());
+    for item in posts.sort_by_date().iter() {
+        let post = item.post;
+        println!("{}", post.slug());
+    }
+}
